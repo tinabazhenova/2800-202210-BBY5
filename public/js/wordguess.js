a72dd6bc1f41c138a56941b2fd77b15e842c32d9
tinabazhenova <tinabazhenov@gmail.com>
2022-05-11 16:05:33 -0700

    Checked if the hardcoded word is the same word as user entered.

--- public/js/wordguess.js
+++ public/js/wordguess.js
@@ -12,24 +12,33 @@ ready(function() {
 
     let position = 0;
     let word = 0;
+    let hardCodedWord = 'youth'.toUpperCase();
+    let tempEnteredWord = '';
 
     window.addEventListener('keydown', function(event) {
         if (position < 5) {
-            if (event.keyCode >= 65 && event.keyCode <= 90) {
-                letters[word * 5 + position].innerHTML = event.key;
-                position++;
+            if (event.keyCode >= 65 && event.keyCode <= 90) { //checks if the user entered a letter
+                letters[word * 5 + position].innerHTML = event.key; // fill array in a line
+                tempEnteredWord += event.key.toUpperCase(); //records a letter into string
+                position++; // increase position
 
-            } else if (event.keyCode == 8 && position != 0) {
-                position--;
-                letters[word * 5 + position].innerHTML = '';
+            } else if (event.keyCode == 8 && position != 0) { // if user hits BS 
+                position--; // decrease position
+                letters[word * 5 + position].innerHTML = ''; //rewrite an indec in an array with empty char
             } else {
-                this.alert('Enter the letter from A- Z');
+                this.alert('Enter the letter from A- Z'); //the user entered the worng character
             }
         } else {
-            if (event.keyCode == 13) {
-                if (word < 4) {
-                    ++word;
-                    position = 0;
+            if (event.keyCode == 13) { //the user hit enter
+                console.log(tempEnteredWord);
+                if (hardCodedWord == tempEnteredWord) {
+                    console.log("tempEnteredWord: " + tempEnteredWord + ", hardCodedWord: " + hardCodedWord);
+                    this.alert('Victory');
+
+                } else if (word < 4) {
+                    ++word; // we give one more option to enter the word
+                    position = 0; //start the position from 0
+                    tempEnteredWord = '';
                 } else {
                     this.alert('Game over');
                 }
