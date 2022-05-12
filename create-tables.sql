41a7d8ee156cd45d1ea336529e20b4f0a8bea83f
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-05-12 14:31:42 -0700

    added more phrases/meanings to db, and table headings to count score

--- create-tables.sql
+++ create-tables.sql
@@ -9,6 +9,22 @@ CREATE TABLE BBY_5_user (
   last_name VARCHAR(50),
   password VARCHAR(50),
   is_admin TINYINT(1),
+  bbscore int,
+  xscore int,
+  yscore int,
+  zscore int,
   PRIMARY KEY (ID)
+);
+
+CREATE TABLE BBY_05_master (
+  word_ID int NOT NULL AUTO_INCREMENT,
+  phrase VARCHAR(50) NOT NULL,
+  meaning VARCHAR(200),
+  history VARCHAR(500),
+  bbvalue int,
+  xvalue int,
+  yvalue int,
+  zvalue int,
+  PRIMARY KEY (word_ID)
 );
 
