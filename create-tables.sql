bc95b8c55c0c5798144a9be35516b0c090de4ed3
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-05-12 10:53:20 -0700

    added media queries and functions to show random phrase + meaning on the page, and 2 random meanings. still need to add score function

--- create-tables.sql
+++ create-tables.sql
@@ -2,27 +2,27 @@
 -- interpreted by the database)
 
 
-CREATE TABLE user (
+CREATE TABLE BBY_05_user (
   ID int NOT NULL AUTO_INCREMENT,
   user_name VARCHAR(50),
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   password VARCHAR(50),
+  bbscore int,
   xscore int,
   yscore int,
   zscore int,
-  bbscore int,
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE master (
+CREATE TABLE BBY_05_master (
   word_ID int NOT NULL AUTO_INCREMENT,
   phrase VARCHAR(50) NOT NULL,
   meaning VARCHAR(200),
   history VARCHAR(500),
+  bbvalue int,
   xvalue int,
   yvalue int,
   zvalue int,
-  bbvalue int,
   PRIMARY KEY (word_ID)
 );
--- public/js/wordmatch.js
+++ public/js/wordmatch.js
@@ -1,12 +1,54 @@
-
 function showDetails() {
-    for (var i = 0; i < arguments.length; i++) {
-      var e = document.getElementById(arguments[i]);
-      e.style.display = e.style.display == "block" ? "none" : "block";
-    }
+  for (var i = 0; i < arguments.length; i++) {
+    var e = document.getElementById(arguments[i]);
+    e.style.display = e.style.display == "block" ? "none" : "block";
   }
+}
 
-  function displayPhrase() {
-    const genPhrase = document.getElementById("div3")
-    genPhrase.innerHTML += results[0].phrase;
-  }
+function getPhrase() {
+  const genPhrase = document.getElementById("div3")
+  const answer = document.getElementById("div" + Math.random() * 3 + 4)
+  connection.connect();
+  connection.query('SELECT * FROM BBY_5_user ORDER BY rand() LIMIT 1;'),
+    function (error, results, fields) {
+      if(error) {
+        console.log(error);
+      }
+      genPhrase.innerHTML += results[0].phrase;
+
+      
+    }
+}
+getPhrase()
+
+
+// May not work asynchronously with getPhrase(). Might need to add promise or await function
+app.get('/wordmatch', function (req, res) {
+  connection.connect();
+  connection.query('SELECT * FROM BBY_5_user ORDER BY rand() LIMIT 2;', function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    }
+    console.log('Rows returned are: ', results);
+    res.send({
+      status: "success",
+      rows: results
+    });
+    for (j=0; j<results.length; j++) {
+      //if (element is empty)
+      document.getElementById("div" + j + 4).innerHTML = results[j].meaning;
+      results[j].addEventListener("click", guessMeaning());
+    }
+   
+  });
+  connection.end();
+});
+
+// Need to add function to change CSS to green on correct guess, and add score to player's count
+function guessMeaning() {
+
+}
+
+// Need to add function to play again
+
+// Optional: add function to display history of the word after guessing
