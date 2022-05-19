4154a14de41f96f5007b768ea5814d89c9caf3b4
tinabazhenova <tinabazhenov@gmail.com>
2022-05-18 23:13:02 -0700

    Added a draft for switching focus between crossword cells

--- public/js/crossword.js
+++ public/js/crossword.js
@@ -12,15 +12,28 @@ ready(function() {
     let rects = new Array();
     let rect = document.getElementById("box0");
     let crossword = document.getElementById("crossword0");
+    rect.setAttribute("row", 0);
+    rect.setAttribute("col", 0);
     rects.push(rect);
-    for (let i = 1; i < 5 * 7; ++i) {
-        rects.push(rect.cloneNode(true));
+    let rows = 5;
+    let cols = 7;
+    for (let i = 1; i < rows * cols; ++i) {
+        let newNode = rect.cloneNode(true);
+        newNode.setAttribute("row", parseInt(i / cols));
+        newNode.setAttribute("col", i % cols);
+        rects.push(newNode);
         crossword.appendChild(rects[i]);
     }
-})
+    for (let i = 1; i < rows * cols; ++i) {
+        rects[i].addEventListener("input", function() {
+            var self = $(this);
+            console.log("input received");
+            let focusRow = 2;
+            let focusCol = 2;
+            let str = 'input[row="' + focusRow + '"][col="' + focusCol + '"]';
 
-$('#crossword0').on('input', function() {
-    var self = $(this)
-    console.log("input received")
-    
-});
+            let newFocus = $(str);
+            newFocus[0].focus();
+        })
+    }
+})
