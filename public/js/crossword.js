510f7dabb6caa31ab0827a8f411ca0df239828a0
tinabazhenova <tinabazhenov@gmail.com>
2022-05-19 23:31:38 -0700

    Deleted commented code.

--- public/js/crossword.js
+++ public/js/crossword.js
@@ -38,45 +38,6 @@ ready(function() {
                 newFocus[0].focus();
                 newFocus[0].select();
             }
-            
-            /*
-            let focusRow = 2;
-            let focusCol = 2;
-            let str = 'input[row="' + focusRow + '"][col="' + focusCol + '"]';
-
-            let newFocus = $(str);
-            newFocus[0].focus();
-            */
-
         })
     }
-    /*
-    let rects = new Array();
-    let rect = document.getElementById("box0");
-    let crossword = document.getElementById("crossword0");
-    rect.setAttribute("row", 0);
-    rect.setAttribute("col", 0);
-    rects.push(rect);
-    let rows = 5;
-    let cols = 7;
-    for (let i = 1; i < rows * cols; ++i) {
-        let newNode = rect.cloneNode(true);
-        newNode.setAttribute("row", parseInt(i / cols));
-        newNode.setAttribute("col", i % cols);
-        rects.push(newNode);
-        crossword.appendChild(rects[i]);
-    }
-    for (let i = 1; i < rows * cols; ++i) {
-        rects[i].addEventListener("input", function() {
-            var self = $(this);
-            console.log("input received");
-            let focusRow = 2;
-            let focusCol = 2;
-            let str = 'input[row="' + focusRow + '"][col="' + focusCol + '"]';
-
-            let newFocus = $(str);
-            newFocus[0].focus();
-        })
-    }
-    */
 })
